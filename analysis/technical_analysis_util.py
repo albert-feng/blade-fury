8e1a55dd8c3f4cb9889cc235c3d7f40b6adb1f63
Albert Feng <fwg1989@gmail.com>
2017-01-11 12:38:32 +0800

    fix bug in set index

--- analysis/technical_analysis_util.py
+++ analysis/technical_analysis_util.py
@@ -29,7 +29,8 @@ def format_trading_data(sdt):
 
 def calculate_macd(df, short_ema, long_ema, dif_ema):
     if isinstance(df, DataFrame):
-        df = df.set_index(['date'])
+        if df.index.name != 'date':
+            df = df.set_index(['date'])
         df['short_ema'] = df['price'].ewm(span=short_ema).mean()
         df['long_ema'] = df['price'].ewm(span=long_ema).mean()
         df['dif'] = df['short_ema'] - df['long_ema']
@@ -42,7 +43,8 @@ def calculate_macd(df, short_ema, long_ema, dif_ema):
 
 def calculate_ma(df, short_ma, long_ma):
     if isinstance(df, DataFrame):
-        df = df.set_index(['date'])
+        if df.index.name != 'date':
+            df = df.set_index(['date'])
         df['short_ma'] = df['price'].rolling(window=short_ma, center=False).mean()
         df['long_ma'] = df['price'].rolling(window=long_ma, center=False).mean()
         df['diff_ma'] = df['short_ma'] - df['long_ma']
