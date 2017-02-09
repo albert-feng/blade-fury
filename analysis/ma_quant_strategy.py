d81734640d1f97b02e72bc0981806f61c91ffacf
albert <fwg1989@gmail.com>
2017-02-10 00:01:57 +0800

    add real time in ma macd_kdj

--- analysis/ma_quant_strategy.py
+++ analysis/ma_quant_strategy.py
@@ -11,7 +11,7 @@ from pandas import DataFrame
 from logger import setup_logging
 from models import QuantResult as QR, StockDailyTrading as SDT
 from analysis.technical_analysis_util import calculate_ma, format_trading_data, check_duplicate_strategy
-from analysis.technical_analysis_util import start_quant_analysis
+from analysis.technical_analysis_util import start_quant_analysis, collect_stock_daily_trading, display_quant
 
 
 query_step = 100  # 一次从数据库中取出的数据量
@@ -21,6 +21,7 @@ def quant_stock(stock_number, stock_name, **kwargs):
     short_ma = kwargs['short_ma']
     long_ma = kwargs['long_ma']
     qr_date = kwargs['qr_date']
+    real_time = kwargs.get('real_time', False)
     if short_ma <= long_ma:
         strategy_direction = 'long'
         quant_count = long_ma + 5
@@ -37,6 +38,15 @@ def quant_stock(stock_number, stock_name, **kwargs):
     if float(sdt[0].increase_rate.replace('%', '')) > 9:
         return
 
+    if real_time:
+        today_sdt = SDT.objects(date=kwargs['qr_date'])
+        if kwargs['qr_date'] == datetime.date.today() and not today_sdt:
+            today_trading = kwargs.get('today_trading', {})
+            if not today_trading.get(stock_number):
+                return
+
+            sdt = list(sdt)
+            sdt.insert(0, today_trading.get(stock_number))
     trading_data = format_trading_data(sdt)
     if not trading_data:
         return
@@ -51,8 +61,12 @@ def quant_stock(stock_number, stock_name, **kwargs):
             strategy_direction=strategy_direction, strategy_name=strategy_name,
             init_price=today_ma['close_price']
         )
+        if real_time:
+            return qr
         if not check_duplicate_strategy(qr):
             qr.save()
+            return qr
+    return ''
 
 
 def setup_argparse():
@@ -60,9 +74,9 @@ def setup_argparse():
     parser.add_argument(u'-s', action=u'store', dest='short_ma', required=True, help=u'短期均线数')
     parser.add_argument(u'-l', action=u'store', dest='long_ma', required=True, help=u'长期均线数')
     parser.add_argument(u'-t', action=u'store', dest='qr_date', required=False, help=u'计算均线的日期')
+    parser.add_argument(u'-r', action=u'store_true', dest='real_time', required=False, help=u'是否实时计算')
 
     args = parser.parse_args()
-
     if args.qr_date:
         try:
             qr_date = datetime.datetime.strptime(args.qr_date, '%Y-%m-%d')
@@ -72,10 +86,17 @@ def setup_argparse():
     else:
         qr_date = datetime.date.today()
 
-    return int(args.short_ma), int(args.long_ma), qr_date
+    return int(args.short_ma), int(args.long_ma), qr_date, args.real_time
 
 
 if __name__ == '__main__':
     setup_logging(__file__, logging.WARNING)
-    short_ma, long_ma, qr_date = setup_argparse()
-    start_quant_analysis(short_ma=short_ma, long_ma=long_ma, qr_date=qr_date, quant_stock=quant_stock)
+    short_ma, long_ma, qr_date, real_time = setup_argparse()
+    today_trading = {}
+    if real_time:
+        today_trading = collect_stock_daily_trading()
+
+    real_time_res = start_quant_analysis(short_ma=short_ma, long_ma=long_ma, qr_date=qr_date, quant_stock=quant_stock,
+                                         real_time=real_time, today_trading=today_trading)
+    if real_time_res and real_time:
+        display_quant(real_time_res)
--- analysis/macd_kdj_strategy.py
+++ analysis/macd_kdj_strategy.py
@@ -10,8 +10,8 @@ from pandas import DataFrame
 
 from logger import setup_logging
 from models import QuantResult as QR, StockDailyTrading as SDT
-from analysis.technical_analysis_util import format_trading_data, check_duplicate_strategy
-from analysis.technical_analysis_util import calculate_macd, calculate_kdj, start_quant_analysis
+from analysis.technical_analysis_util import format_trading_data, check_duplicate_strategy, collect_stock_daily_trading
+from analysis.technical_analysis_util import calculate_macd, calculate_kdj, start_quant_analysis, display_quant
 
 
 step = 100  # 一次从数据库取出打股票数量
@@ -19,6 +19,7 @@ ema_volume = 150
 
 
 def quant_stock(stock_number, stock_name, **kwargs):
+    real_time = kwargs.get('real_time', False)
     sdt = SDT.objects(Q(stock_number=stock_number) & Q(today_closing_price__ne=0.0) &
                       Q(date__lte=kwargs['qr_date'])).order_by('-date')[:ema_volume]
     if len(sdt) < ema_volume-50:
@@ -26,6 +27,15 @@ def quant_stock(stock_number, stock_name, **kwargs):
     if float(sdt[0].increase_rate.replace('%', '')) > 9:
         return
 
+    if real_time:
+        today_sdt = SDT.objects(date=kwargs['qr_date'])
+        if kwargs['qr_date'] == datetime.date.today() and not today_sdt:
+            today_trading = kwargs.get('today_trading', {})
+            if not today_trading.get(stock_number):
+                return
+
+            sdt = list(sdt)
+            sdt.insert(0, today_trading.get(stock_number))
     trading_data = format_trading_data(sdt)
     df = calculate_macd(DataFrame(trading_data), kwargs['short_ema'], kwargs['long_ema'], kwargs['dif_ema'])
     df = calculate_kdj(df)
@@ -42,14 +52,19 @@ def quant_stock(stock_number, stock_name, **kwargs):
                 init_price=today_analysis['close_price']
             )
 
+            if real_time:
+                return qr
             if isinstance(qr, QR):
                 if not check_duplicate_strategy(qr):
                     qr.save()
+                    return qr
+    return ''
 
 
 def setup_argparse():
     parser = argparse.ArgumentParser(description=u'根据macd_kdj来选股')
     parser.add_argument(u'-t', action=u'store', dest='qr_date', required=False, help=u'计算均线的日期')
+    parser.add_argument(u'-r', action=u'store_true', dest='real_time', required=False, help=u'是否实时计算')
     args = parser.parse_args()
 
     if args.qr_date:
@@ -60,7 +75,7 @@ def setup_argparse():
             raise e
     else:
         qr_date = datetime.date.today()
-    return qr_date
+    return qr_date, args.real_time
 
 
 if __name__ == '__main__':
@@ -68,6 +83,12 @@ if __name__ == '__main__':
     short_ema = 12
     long_ema = 26
     dif_ema = 9
-    qr_date = setup_argparse()
-    start_quant_analysis(short_ema=short_ema, long_ema=long_ema, dif_ema=dif_ema, qr_date=qr_date,
-                         quant_stock=quant_stock)
+    qr_date, real_time = setup_argparse()
+    today_trading = {}
+    if real_time:
+        today_trading = collect_stock_daily_trading()
+
+    real_time_res = start_quant_analysis(short_ema=short_ema, long_ema=long_ema, dif_ema=dif_ema, qr_date=qr_date,
+                                         quant_stock=quant_stock, real_time=real_time, today_trading=today_trading)
+    if real_time_res and real_time:
+        display_quant(real_time_res)
