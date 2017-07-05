1f100c6be7565185b6532aceb871cdc0d4e20c10
fengweigang <fengweigang@alibaba-inc.com>
2017-07-05 14:13:53 +0800

    add reverse in cal price

--- analysis/cal_price_volatility.py
+++ analysis/cal_price_volatility.py
@@ -23,10 +23,10 @@ def fetch_stock_price(stock_number, date):
     if sdt:
         return sdt.first().today_closing_price
     else:
-        return
+        return 0
 
 
-def start_calculate(start_date, end_date):
+def start_calculate(start_date, end_date, reverse=False):
     if not isinstance(start_date, datetime.datetime) or not isinstance(end_date, datetime.datetime):
         return
 
@@ -40,8 +40,12 @@ def start_calculate(start_date, end_date):
             continue
 
         price_diff = end_price - start_price
-        if price_diff < 0:
-            continue
+        if reverse:
+            if price_diff < 0:
+                continue
+        else:
+            if price_diff > 0:
+                continue
 
         price_volatility.append({
             'stock_number': i.stock_number,
@@ -51,22 +55,26 @@ def start_calculate(start_date, end_date):
             'increase_rate': round(price_diff/start_price, 4)
         })
 
-    price_volatility = sorted(price_volatility, key=lambda x: x['increase_rate'], reverse=True)
+    if not price_volatility:
+        print 'No Output'
+        return
+
+    price_volatility = sorted(price_volatility, key=lambda x: x['increase_rate'], reverse=reverse)
     count = len(price_volatility)
 
     print '------------------%s--------------------' % count
-    df = DataFrame(price_volatility[:display_count]).set_index(['stock_number']).reindex(columns=['stock_name', 'start_price',
-                                                                      'end_price', 'increase_rate'])
+    df = DataFrame(price_volatility[:display_count]).set_index(['stock_number'])\
+        .reindex(columns=['stock_name', 'start_price', 'end_price', 'increase_rate'])
     pd.set_option('display.max_rows', display_count + 10)
     print df
     pd.reset_option('display.max_rows')
-
 
 
 def setup_argparse():
     parser = argparse.ArgumentParser(description=u'计算价格变化并排行')
     parser.add_argument(u'-s', action=u'store', dest='start_date', required=True, help=u'开始时间')
     parser.add_argument(u'-e', action=u'store', dest='end_date', required=True, help=u'结束时间')
+    parser.add_argument(u'-r', action=u'store_true', dest='reverse', required=False, help=u'倒序计算')
 
     args = parser.parse_args()
     try:
@@ -76,11 +84,10 @@ def setup_argparse():
         print 'Wrong date form'
         raise e
 
-    return start_date, end_date
+    return start_date, end_date, args.reverse
 
 
 if __name__ == '__main__':
     setup_logging(__file__, logging.WARNING)
-    start_date, end_date = setup_argparse()
-    start_calculate(start_date, end_date)
-
+    start_date, end_date, reverse = setup_argparse()
+    start_calculate(start_date, end_date, reverse)
